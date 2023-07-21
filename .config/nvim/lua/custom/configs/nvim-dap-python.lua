2e5e37dfbf14b4a3c3d921db56096bd0d16991a2
Mike Bijl <mike@gorilla.co>
2023-07-21 20:54:01 +0200

    removed NvChad

--- .config/nvim/lua/custom/configs/nvim-dap-python.lua
+++
@@ -1,4 +0,0 @@
-local dap_python = require("dap-python")
-local python_path = os.getenv("VIRTUAL_ENV") .. "/bin/python"
-
-dap_python.setup(python_path, { include_configs = true })
--- .config/nvim/lua/custom/configs/telescope.lua
+++
@@ -1,8 +0,0 @@
-local options = {
-  defaults = {
-    -- has to be configured for iterm to show correctly
-    prompt_prefix = "   ",
-  }
-}
-
-return options
