64c903d44c31f97b35c76d3afbbdf2c83a2ae837
Mike Bijl <bijlmike3@gmail.com>
2025-08-27 14:25:31 +0200

    fix(lsp): installed asdf to support nodejs
    My pyright LSP didn't work anymore, so I wanted to perform a
    MasonUpdate. Apparently this needs some version of npm installed so I
    dove in the rabbit hole to properly install node in my fish shell.
    
    I think I added too much crap, but got my pyright updated & it stopped
    crashing.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,26 @@ if status is-interactive
     set -xg LC_ALL en_US.UTF-8
     set -xg LC_CTYPE en_US.UTF-8
     set -xg PYENV_ROOT $HOME/.pyenv
+    set -xg NVM_DIR (brew --prefix nvm)
 
     set -xg HOMEBREW_AUTO_UPDATE_SECS 86400
     set -xg HOMEBREW_NO_COLOR 1
     set -xg HOMEBREW_NO_EMOJI 1
 
-    fish_add_path ~/bin ~/go/bin ~/.cargo/bin ~/.local/bin /opt/homebrew/bin /usr/local/opt/libpq/bin
+    fish_add_path ~/bin ~/go/bin ~/.cargo/bin ~/.local/bin /opt/homebrew/bin /usr/local/opt/libpq/bin /opt/homebrew/opt/gawk/libexec/gnubin
+    # ASDF configuration code
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
 
     # https://github.com/pyenv/pyenv/issues/32
     pyenv init - fish | source
